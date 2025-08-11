2fc0e8f7c611f3cc90462641ff54cc81458461de
CRIMSONCARNAGEE <sohibaorazboyeva@gmail.com>
2025-08-11 22:01:01 +0500

    feat:expres integrations

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import dotenv from 'dotenv'
 dotenv.config();
+import app from './app';
 
 import mongoose from 'mongoose'
 import { log } from 'node:console';
@@ -7,6 +8,10 @@ mongoose
 .connect(process.env.MONGO_URL as string, {})
 .then((data) => {
   console.log('MongoDb connection succus');
+  const PORT = process.env.PORT ?? 3000;
+  app.listen(PORT, function () {
+    console.log(`This server is running succesfully on port: ${PORT}`);
+  })
 })
 .catch((err) => {
   console.log('ERROR:', err);
