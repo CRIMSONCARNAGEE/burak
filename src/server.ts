c3c7cc165603a8f66ebcf84585b3f28a3da93046
CRIMSONCARNAGEE <sohibaorazboyeva@gmail.com>
2025-08-11 10:04:55 +0500

    feat:integration of envirommental veriables

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,3 @@
-console.log('EXCUTED!');
-import moment from "moment";
-
-const currentTime = moment().format('YYYY-MM-DD')
-console.log(currentTime);
-
-
-const person: string = "Martin"
-const count: number = 100
+import dotenv from 'dotenv'
+import { log } from 'node:console';
+dotenv.config();
