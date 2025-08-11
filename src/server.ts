4998a6ea7f585d16b8443074699bc79e9770cd8d
CRIMSONCARNAGEE <sohibaorazboyeva@gmail.com>
2025-08-11 21:23:17 +0500

    feat:connect mongodb via mongoose

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,13 @@
 import dotenv from 'dotenv'
-import { log } from 'node:console';
 dotenv.config();
+
+import mongoose from 'mongoose'
+import { log } from 'node:console';
+mongoose
+.connect(process.env.MONGO_URL as string, {})
+.then((data) => {
+  console.log('MongoDb connection succus');
+})
+.catch((err) => {
+  console.log('ERROR:', err);
+})
