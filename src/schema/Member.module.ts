a8b095f340fcc555b370c03aa2ed29b7a3d93b01
CRIMSONCARNAGEE <sohibaorazboyeva@gmail.com>
2025-08-12 22:34:37 +0500

    feat: member schema module

---
+++ src/schema/Member.module.ts
@@ -0,0 +1,47 @@
+import mongoose, { Schema } from "mongoose";
+import { MemberStatus, MemberType } from '../libs/enums/member.enam'
+
+const memeberSchema = new Schema({
+  memberType: {
+    type: String,
+    enum: MemberType,
+    default: MemberType.USER
+  },
+  MemberStatus: {
+    type: String,
+    enum: MemberStatus,
+    default: MemberStatus.ACTIVE
+  },
+  MemberNick: {
+    type: String,
+    index: {unique: true, sparse: true},
+    required: true
+  },
+  MemberPhone: {
+    type: String,
+    index: {unique: true, sparse: true},
+    required:true
+  },
+  MemberPassword: {
+    type: String,
+    select: false,
+    required:true
+  },
+  MemberAddres: {
+    type: String
+  },
+    MemberDesc: {
+    type: String
+  },
+    MemberImage: {
+    type: String
+  },
+  MemberPoints: {
+    type: Number,
+    default: 0
+  },
+},
+  { timestamps: true }
+);
+
+export default mongoose.model('Member',memeberSchema)
