9ce854c238f7728f0cdf3e31f0789e4cf98e169e
CRIMSONCARNAGEE <sohibaorazboyeva@gmail.com>
2025-09-13 16:38:34 +0500

    feat: create product controller and product routinf apis

---
+++ src/controllers/product.controller.ts
@@ -0,0 +1,46 @@
+import express, { Request, Response } from "express";
+import Errors from "../libs/Errors";
+import { T } from "../libs/types/common";
+import ProductService from "../models/Product.service";
+
+
+const productService = new ProductService()
+
+const productController: T = {};
+productController.getAllProducts = async (req: Request, res: Response) => {
+  try {
+    console.log("getAllProducts");
+    res.render('products')
+  }catch (err) {
+    console.log('ERROR getAllProducts:', err);
+    if(err instanceof Errors) res.status(err.code).json(err);
+    else res.status(Errors.standart.code).json(Errors.standart)
+    // res.json({  })
+  }
+}
+
+productController.createNewProducts = async (req: Request, res: Response) => {
+  try {
+    console.log("getAllProducts");
+
+  }catch (err) {
+    console.log('ERROR createNewProducts:', err);
+    if(err instanceof Errors) res.status(err.code).json(err);
+    else res.status(Errors.standart.code).json(Errors.standart)
+    // res.json({  })
+  }
+}
+
+productController.updateChoosenProducts = async (req: Request, res: Response) => {
+  try {
+    console.log("getAllProducts");
+
+  }catch (err) {
+    console.log('ERROR updateChoosenProducts:', err);
+    if(err instanceof Errors) res.status(err.code).json(err);
+    else res.status(Errors.standart.code).json(Errors.standart)
+    // res.json({  })
+  }
+}
+
+export default productController;
--- src/router-admin.ts
+++ src/router-admin.ts
@@ -1,20 +1,24 @@
 import express from "express";
 const routerAdmin = express.Router();
 import restaurantController from "./controllers/restaurant.controller";
+import productController from "./controllers/product.controller";
 
 /* Restaurant */
 routerAdmin.get('/', restaurantController.goHome);
 routerAdmin
-    .get('/login', restaurantController.getLogin)
-    .post('/login', restaurantController.processLogin);
+.get('/login', restaurantController.getLogin)
+.post('/login', restaurantController.processLogin);
 routerAdmin
-  .get('/signup', restaurantController.getSignUp)
-  .post('/signup', restaurantController.processSignUp);
-  routerAdmin
-    .get('/logout', restaurantController.logout)
+.get('/signup', restaurantController.getSignUp)
+.post('/signup', restaurantController.processSignUp);
+routerAdmin
+.get('/logout', restaurantController.logout)
 
-  routerAdmin
-    .get('/check-me', restaurantController.checkAuthSession)
+routerAdmin
+.get('/check-me', restaurantController.checkAuthSession)
 /* Product */
+routerAdmin.get('/product/all', productController.getAllProducts)
+routerAdmin.post('/product/create', productController.createNewProducts)
+routerAdmin.post('/product/::id', productController.updateChoosenProducts)
 /*User */
 export default routerAdmin;
